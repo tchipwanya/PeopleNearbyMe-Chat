43062036f1185457237a0a3096deb9ba82d1e632
Teddy Knox <teddyknox@gmail.com>
2012-05-17 20:15:32 -0400

    what up

--- client/facebook.js
+++ client/facebook.js
@@ -1,12 +1,13 @@
 window.fbAsyncInit = function() {
 	FB.init({
-		appId			:'262579080505640', // App ID
-		channelUrl		: '/channel', // Channel File
+		appId			:'262579080505640', // PRODUCTION
+//		appId			:'408731785826423', // DEVELOPMENT		
 		status			: true, // check login status
 		cookies			: true, // enable cookies to allow the server to access the session
 		xfbml			: true  // parse XFBML
 	});
 };
+
 // Load the SDK Asynchronously
 (function(d){
 	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
--- public/javascripts/client.js
+++ public/javascripts/client.js
@@ -1,4 +1,30 @@
-/* client.js subfile 1 */
+window.fbAsyncInit = function() {
+	FB.init({
+		appId			:'262579080505640', // App ID
+		status			: true, // check login status
+		cookies			: true, // enable cookies to allow the server to access the session
+		xfbml			: true  // parse XFBML
+	});
+};
+// Load the SDK Asynchronously
+(function(d){
+	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
+	if (d.getElementById(id)) {return;}
+	js = d.createElement('script'); js.id = id; js.async = true;
+	js.src = "//connect.facebook.net/en_US/all.js";
+	ref.parentNode.insertBefore(js, ref);
+}(document));
+
+function fbLogin() {
+	FB.login(function(response) {
+		if (response.authResponse) {
+			FB.api('/me', function(response) {
+				CONFIG.fbid = response.name;
+			});
+		} else {
+		}
+	});
+};/* client.js subfile 1 */
 
 var CONFIG = {	alias: null,   // set in onConnect,
 				fbid: null,
@@ -540,31 +566,4 @@ $(document).ready(function() {
 });
 
 //if we can, notify the server that we're going away.
-$(window).unload(part);window.fbAsyncInit = function() {
-	FB.init({
-		appId			:'262579080505640', // App ID
-		channelUrl		: '/channel', // Channel File
-		status			: true, // check login status
-		cookies			: true, // enable cookies to allow the server to access the session
-		xfbml			: true  // parse XFBML
-	});
-};
-// Load the SDK Asynchronously
-(function(d){
-	var js, id = 'facebook-jssdk', ref = d.getElementsByTagName('script')[0];
-	if (d.getElementById(id)) {return;}
-	js = d.createElement('script'); js.id = id; js.async = true;
-	js.src = "//connect.facebook.net/en_US/all.js";
-	ref.parentNode.insertBefore(js, ref);
-}(document));
-
-function fbLogin() {
-	FB.login(function(response) {
-		if (response.authResponse) {
-			FB.api('/me', function(response) {
-				CONFIG.fbid = response.name;
-			});
-		} else {
-		}
-	});
-};
+$(window).unload(part);
